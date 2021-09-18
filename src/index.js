1990e79a9b417d72c084aa0376c383ff7891187f
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-09-17 21:47:47 -0400

    Added emojipedia starting files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./emojipedia/App";
+import App from "./emojipedia/components/App";
 
 ReactDOM.render(
   <React.StrictMode>
