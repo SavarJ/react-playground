b59df7382eb637bf0ad479377b48066b510cf565
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-10-08 18:21:55 -0400

    Finish emojipedia challenge which was over mapping components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// * Change this line below to view a different project
 import App from "./emojipedia/components/App";
 
 ReactDOM.render(
