a8c355a9b61c8cfbcbc1725e66363a881e97ef21
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-10-08 20:00:54 -0400

    Login starting files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // * Change this line below to view a different project
-import App from "./emojipedia/components/App";
+// import App from "./emojipedia/components/App";
+import App from "./login/components/App";
 
 ReactDOM.render(
   <React.StrictMode>
