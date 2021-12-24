705d8f46fefa0eeb8173740a8672f050fb1e43d9
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-12-24 16:22:55 -0500

    Add starting files for todolist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom";
 // * Change this line below to view a different project
 // import App from "./emojipedia/components/App";
 // import App from "./login/components/App";
-import App from "./clock/components/App";
+// import App from "./clock/components/App";
+import App from "./todolist/App";
 
 ReactDOM.render(
   <React.StrictMode>
